5b1bff40e44f367cbabfec64ef71dc8cd0ef447f
Stephan Gatzka <stephan.gatzka@gmail.com>
2020-08-26 20:08:11 +0200

    Read and handle message type.

--- messages.c
+++ messages.c
@@ -26,22 +26,76 @@
  * SOFTWARE.
  */
 
+#include <stddef.h>
+
 #include "cio/buffered_stream.h"
 #include "cio/compiler.h"
 #include "cio/error_code.h"
 
 #include "jet_client.h"
 #include "messages.h"
+#include "sj_log.h"
 
-void read_jet_message(struct cio_buffered_stream *bs, void *handler_context, enum cio_error err)
+static void handle_request(struct jet_client *client)
 {
-	//struct jet_client *client = (struct jet_client *)handler_context;
-    (void)handler_context;
+	(void)client;
+}
 
-	if (cio_unlikely(err != CIO_SUCCESS)) {
+static void handle_response(struct jet_client *client)
+{
+	(void)client;
+}
+
+static void message_type_read(struct cio_buffered_stream *bs, void *handler_context, enum cio_error err, struct cio_read_buffer *buffer, size_t num_bytes)
+{
+	(void)num_bytes;
+
+	if (cio_unlikely(err == CIO_EOF)) {
+		sclog_message(&sj_log, SCLOG_INFO, "connection closed by client!");
 		cio_buffered_stream_close(bs);
 		return;
 	}
 
-	cio_buffered_stream_close(bs);
+	if (cio_unlikely(err != CIO_SUCCESS)) {
+		sclog_message(&sj_log, SCLOG_ERROR, "reading message type failed!");
+		cio_buffered_stream_close(bs);
+		return;
+	}
+
+	uint8_t message_type;
+	memcpy(&message_type, cio_read_buffer_get_read_ptr(buffer), sizeof(message_type));
+	cio_read_buffer_consume(buffer, sizeof(message_type));
+
+	struct jet_client *client = (struct jet_client *)handler_context;
+
+	switch ((enum jet_message)message_type) {
+	case MESSAGE_REQUEST:
+		handle_request(client);
+		break;
+
+	case MESSAGE_RESPONSE:
+		handle_response(client);
+		break;
+
+	default:
+		sclog_message(&sj_log, SCLOG_ERROR, "unknown message type sent by client!");
+		cio_buffered_stream_close(bs);
+		break;
+	}
+}
+
+void read_jet_message(struct cio_buffered_stream *bs, void *handler_context, enum cio_error err)
+{
+	if (cio_unlikely(err != CIO_SUCCESS)) {
+		sclog_message(&sj_log, SCLOG_ERROR, "write before reading message type failed!");
+		cio_buffered_stream_close(bs);
+		return;
+	}
+
+	struct jet_client *client = (struct jet_client *)handler_context;
+	err = cio_buffered_stream_read_at_least(bs, &client->rb, 1, message_type_read, client);
+	if (cio_unlikely(err != CIO_SUCCESS)) {
+		sclog_message(&sj_log, SCLOG_ERROR, "Could not start reading message type!");
+		cio_buffered_stream_close(bs);
+	}
 }
